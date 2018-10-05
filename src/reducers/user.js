55603f6ba07a351f09f889540ab1cd753b0e169e
Aman Ibrahim <amanmibra@gmail.com>
2018-10-05 19:23:36 -0400

    Added the following: - Redux (user reducer) - Redux Dev Tools - Redux Persistance

---
+++ src/reducers/user.js
@@ -0,0 +1,86 @@
+const initialState = {
+  data: {},
+  isLoggedIn: false,
+  isLoading: false,
+  error: false,
+  errorMessage: '',
+};
+
+const user = (state=initialState, action) => {
+  switch (action.type) {
+    // POST /login
+    case 'BEGIN_LOGIN': {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+
+    case 'FAILED_LOGIN': {
+      return {
+        ...initialState,
+        error: true,
+        errorMessage: action.payload.response.data.message,
+        errorObject: action.payload,
+      };
+    }
+
+    case 'END_LOGIN': {
+      return {
+        ...state,
+        data: action.payload.data.data,
+        isLoggedIn: true,
+      };
+    }
+
+    case 'BEGIN_SIGNUP': {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+
+    case 'FAILED_SIGNUP': {
+      return {
+        ...initialState,
+        error: true,
+        errorMessage: action.payload.response.data.message,
+        errorObject: action.payload,
+      };
+    }
+
+    case 'END_SIGNUP': {
+      return {
+        ...state,
+        data: action.payload.data.data,
+        isLoggedIn: true,
+      };
+    }
+
+    case 'BEGIN_LOGOUT': {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+
+    case 'FAILED_LOGOUT': {
+      return {
+        ...state,
+        isLoading: false,
+        error: true,
+        errorMessage: action.payload,
+      };
+    }
+
+    case 'END_LOGOUT': {
+      return initialState;
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+export default user;
