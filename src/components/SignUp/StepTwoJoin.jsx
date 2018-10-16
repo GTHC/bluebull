0e9191ad7e6eb0c3287b6fac1793d5440c26b332
Aman Ibrahim <amanmibra@gmail.com>
2018-10-16 11:42:23 -0400

    Begin adding AutoSuggest component for join team

--- src/components/SignUp/StepTwoJoin.jsx
+++ src/components/SignUp/StepTwoJoin.jsx
@@ -6,14 +6,20 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import GroupIcon from '@material-ui/icons/Group';
 
+// components
+import AutoComplete from './AutoComplete';
+
 class StepTwoJoin extends Component {
 
   render() {
     return (
       <div>
         <DialogTitle>{
-          <div style={{ textAlign: 'center'}}>Join Team <GroupIcon /></div>
+          <div style={{ textAlign: 'center' }}>Join Team <GroupIcon /></div>
         }</DialogTitle>
+        <DialogContent>
+          <AutoComplete teams={this.props.teams} />
+        </DialogContent>
       </div>
     );
   }
