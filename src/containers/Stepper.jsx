20703282c59e9a9a0bc2ff8f51ce6976c8932777
Rikki Kendall <yrk3@duke.edu>
2018-12-01 14:11:17 -0500

    steps

--- src/containers/Stepper.jsx
+++ src/containers/Stepper.jsx
@@ -1,91 +1,14 @@
 import React, { Component } from 'react';
-// Stepper
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import StepContent from '@material-ui/core/StepContent';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
-  root: {
-    width: '90%',
-  },
-  button: {
-    marginTop: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-  },
-  actionsContainer: {
-    marginBottom: theme.spacing.unit * 2,
-  },
-  resetContainer: {
-    padding: theme.spacing.unit * 3,
-  },
-  card: {
-    maxWidth: 700,
-  },
-  media: {
-    height: 300,
-  },
-});
+// semantic ui components
+import { Button, Form, Step, Divider } from 'semantic-ui-react';
 
-function TabContainer(props) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
-  );
-};
+// sub-components
+import UserSignUp from './signup/UserSignUp';
+import TeamSignUp from './signup/TeamSignUp';
+import AllSet from './signup/AllSet';
 
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-function getSteps() {
-  return ['K-Ville 101', 'Step 2', 'Step 3'];
-};
-
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return `This is the beginning of a step-by-step process on how to go about all things K-Ville!`;
-    case 1:
-      return 'Establish the Captain of your team. Submit your Tenting Roster to the Line Monitors. Create and join the team on our app.';
-    case 2:
-      return `Get a Tent! Make sure you check the specification of the tent for your team and utilize our tools to help you.`;
-    default:
-      return 'Unknown step';
-  }
-};
-
-class VerticalLinearStepper extends React.Component {
-  state = {
-    activeStep: 0,
-  };
-
-  handleNext = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep + 1,
-    }));
-  };
-
-  handleBack = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep - 1,
-    }));
-  };
-
-  handleReset = () => {
-    this.setState({
-      activeStep: 0,
-    });
-  };
-};
-
-class Stepper extends Component {
-
+class SignUpFields extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -105,57 +28,58 @@ class Stepper extends Component {
     this.setState({ value });
   };
 
- render() {
-    const steps = getSteps();
+  render() {
+    const steps = [
+        { key: 'user', icon: 'user', title: 'User Credentials', description: 'Add your email and create an account password.', active: (activeStep === 0) },
+        { key: 'team', active: true, icon: 'users', title: 'Team Information', description: 'Let us know which team you are on!', active: (activeStep === 1) },
+        { key: 'join', disabled: true, icon: 'checkmark box', title: 'All Set!', active: (activeStep === 2), completed: (activeStep === 2) },
+      ];
     const { activeStep } = this.state;
     const { value } = this.state;
 
     return (
-      {activeStep === steps.length && (
       <div>
-      <Stepper activeStep={activeStep} orientation="vertical">
-        {steps.map((label, index) => {
-          return (
-            <Step key={label}>
-              <StepLabel> <h3> {label} </h3> </StepLabel>
-              <StepContent>
-                <Typography>{getStepContent(index)}</Typography>
-                <div>
-                  <div>
-                    <Button
-                      disabled={activeStep === 0}
-                      onClick={this.handleBack}
-                    >
-                      Back
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => {
-                      if (activeStep < 3) {
-                      this.setState({ activeStep: activeStep + 1 })
-                    }
-                    }}
-                    >
-                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                    </Button>
-                  </div>
-                </div>
-              </StepContent>
-            </Step>
-            )
-      })}
-      )
-      </Stepper>
+        <Step.Group fluid items={steps} />
+        <br />
+        <br />
+        <Form>
+          { activeStep === 0 &&
+            <UserSignUp
+              login={login}
+              toggleDisableNext={toggleDisableNext}
+              updateUserInfo={updateUserInfo}
+            />
+          }
+          { activeStep === 1 &&
+            <TeamSignUp
+              login={login}
+              toggleDisableNext={toggleDisableNext}
+              updateTeamInfo={updateTeamInfo}
+            />
+          }
+          { activeStep === 2 &&
+            <AllSet login={login} />
+          }
+
+        </Form>
+        <br />
+        { activeStep < 2 ?
+          <Button.Group fluid>
+            <Button id="back" content='Back' icon='left arrow' labelPosition='left' color="red" onClick={this.handleButtonClick} />
+            <Button.Or />
+            <Button id="next" content='Next' icon='right arrow' labelPosition='right' disabled={login.disableNext} color="green" onClick={this.handleButtonClick} />
+          </Button.Group> :
+          <Button.Group fluid>
+            <Button id="back" content='Back' icon='left arrow' labelPosition='left' color="red" onClick={this.handleButtonClick} />
+            <Button.Or />
+            <Button id="signup" content='Sign Up and Login' icon='sign in' labelPosition='right' color="green" onClick={this.handleButtonClick} />
+          </Button.Group>
+        }
+        <br />
+        <br />
       </div>
-      }
-      // )}
-    )
+    );
   }
-};
-
-export default Stepper;
-
-export {
-  Stepper
 }
+
+export default SignUpFields;
