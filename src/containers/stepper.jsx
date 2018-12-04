7a69d9c3193eea38f7ada7f4342470ac4a83df1a
Rikki Kendall <yrk3@duke.edu>
2018-12-03 22:19:28 -0500

    updated

--- src/containers/stepper.jsx
+++ src/containers/stepper.jsx
@@ -107,25 +107,37 @@ class Stepper extends Component {
           }
           { activeStep === 2 &&
              <div>
-             <img src="https://kenan.ethics.duke.edu/wp-content/uploads/2017/02/tenting-01-300x300.png"/>
-             <Segment.Group>
-                <Segment><h1>Tenting Essentials</h1></Segment>
-                <Segment.Group>
-                  <Segment as='h5' attached>1. Tent </Segment>
-                  <Message warning attached='bottom'>
-                    <Icon name='warning' />
-                        Fire Safe, Large 12 Person Tent
-                  </Message>
-                  <Segment as='h5' attached>2. Palletes </Segment>
-                  <Message attached> (Goes under the tents) </Message>
-                  <Segment as='h5' attached>3. Tarp </Segment>
-                  <Message attached inverted> (Over the tent, helps with heat and weather) </Message>
-                  <Segment as='h5' attached>4. 0 Degree Sleeping Bags </Segment>
-                  <Message attached inverted> (It gets cold in K-Ville) </Message>
-                  <Segment as='h5' attached>5. Flashlights, Lanterns </Segment>
-                  <Message attached inverted> (For the fun late nights in K-Ville)</Message>
-                </Segment.Group>
-              </Segment.Group>
+             <Segment placeholder>
+                <Grid columns={2} stackable textAlign='center'>
+                  <Divider vertical></Divider>
+                  <Grid.Row verticalAlign='middle'>
+                    <Grid.Column>
+                      <Segment><h1>Tenting Essentials</h1></Segment>
+                      <img src="https://kenan.ethics.duke.edu/wp-content/uploads/2017/02/tenting-01-300x300.png"/>
+                    </Grid.Column>
+                    <Grid.Column>
+                    <Segment.Group>
+                      <Segment><h3> The following items: </h3></Segment>
+                      <Segment.Group>
+                        <Segment as='h5' attached>1. Tent </Segment>
+                        <Message warning attached='bottom'>
+                          <Icon name='warning' />
+                              Fire Safe, Large 12 Person Tent
+                        </Message>
+                        <Segment as='h5' attached>2. Palletes </Segment>
+                        <Message attached> (Goes under the tents) </Message>
+                        <Segment as='h5' attached>3. Tarp </Segment>
+                        <Message attached inverted> (Over the tent, helps with heat and weather) </Message>
+                        <Segment as='h5' attached>4. 0 Degree Sleeping Bags </Segment>
+                        <Message attached inverted> (It gets cold in K-Ville) </Message>
+                        <Segment as='h5' attached>5. Flashlights, Lanterns </Segment>
+                        <Message attached inverted> (For the fun late nights in K-Ville)</Message>
+                      </Segment.Group>
+                    </Segment.Group>
+                    </Grid.Column>
+                    </Grid.Row>
+                    </Grid>
+                    </Segment>
              </div>
           }
         <br />
